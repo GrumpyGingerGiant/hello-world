8cc0499047abb80eba3872320b28551fbeafa54a
mathp0455 <mathp0455@EDS-IC0026495.wrdsb.ca>
2017-04-13 10:36:47 -0400

    Entering source code

---
+++ helloWorldTest/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: mathp0455
+ *
+ * Created on April 13, 2017, 10:35 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
